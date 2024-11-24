bac414226ff48c88757cd7ff3bd124fb016c807c
ArjupreetKaur <kaurarju@sheridancollege.ca>
2024-11-24 00:30:32 -0500

    1st change

---
+++ src/student/Student.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author arjup
+ */
+
+public class Student 
+{
+    private String name;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param givenName the name to set
+     */
+    public void setName(String givenName) {
+        name = givenName;
+    }
+    
+    
+}
